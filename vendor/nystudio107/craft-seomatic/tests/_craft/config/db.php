d96be7abc0be4ea0b5f4c6d18d54bbaaf04d4e91
lujayn-new <amir.s.lujayninfoways@gmail.com>
2022-03-09 13:08:11 +0530

    merge data

--- vendor/nystudio107/craft-seomatic/tests/_craft/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'dsn' => getenv('DB_DSN'),
-    'password' => getenv('DB_PASSWORD'),
-    'user' => getenv('DB_USER'),
-    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
-    'schema' => getenv('DB_SCHEMA'),
-];
